40938d3ee299f5539ae584cd9304fef6c9bf6133
J <30843706+Jakir9@users.noreply.github.com>
2023-01-29 21:37:47 +0000

    added background and cleaned up css

--- api.js
+++ api.js
@@ -1,10 +1,13 @@
-// api key: de9bd6f99b628a122f3be3f98428c81c
+let key = "de9bd6f99b628a122f3be3f98428c81c";
+
+
+let location = "Birmingham,UK";
 
 let weather = {
-    'apiKey' : 'de9bd6f99b628a122f3be3f98428c81c',
+   
     fetchWeather : function(){
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q={BIRMINGHAM}&appid={de9bd6f99b628a122f3be3f98428c81c}&units=metric")
+            "https://api.openweathermap.org/data/2.5/weather?q={" + location + "}&appid={" + key + "}&units=metric")
             .then((response) => response.json()) // gets weather data and represents it in json format
             .then((data) => console.log(data)); // displays data from api
     }
