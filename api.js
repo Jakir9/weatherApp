055df6c8eed058ddcc31db447e6af99bc43e42e2
J <30843706+Jakir9@users.noreply.github.com>
2023-02-01 12:54:15 +0000

    Add files via upload

--- api.js
+++ api.js
@@ -9,9 +9,8 @@ const units = "metric";
 var lon = document.getElementById('longitude').value;
 var lat = document.getElementById('latitude').value;
 let lang = 'en';
-let url = "https://api.openweathermap.org/data/3.0/onecall?lat={" + lat + "}&lon={" + lon + "}&appid={"+ key +"}";
+let url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid="+ key;
 
-   
 function getWeather(url) {
     fetch(url)
         .then((response) => response.json()) // gets weather data and represents it in json format
@@ -19,29 +18,34 @@ function getWeather(url) {
 }
 
 
-
 function getLocation() {
    if(navigator.geolocation) {
-    geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition);
 
-    } else { console.log('Geolocation unsupported'); 
+    } else 
+      { 
+        doc.innerHTML ="Geolocation is not supported";
+  }
 }
-}
+
 
 function getCity(){
-   return navigator.geolocation.getCity(getLocation)
+   return navigator.geolocation.getCity(getLocation);
 }
+
 
 function showPosition(position) {
     doc.innerHTML = "Latitude: " + position.coords.latitude +
     "<br> Longitude: " + position.coords.longitude;
   }
 
+  //Last updated
 var date = new Date();
 console.log(date);
 
-var time = date.getTime();
-console.log(time);
+//var time = date.getTime();
+//console.log(time);
+
 //api call for a city: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 
