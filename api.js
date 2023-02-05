4edb0a18f6a4bcef42b8f560f09cd08e051c694c
J <30843706+Jakir9@users.noreply.github.com>
2023-02-05 20:33:48 +0000

    Working App

--- api.js
+++ api.js
@@ -1,51 +1,109 @@
-import 'style.css';
-import 'index.html';
+var key;
 
-let key = "de9bd6f99b628a122f3be3f98428c81c";
+var longitude;
+var latitude; 
+var city;
+key = "de9bd6f99b628a122f3be3f98428c81c";
 
-let doc = document.getElementById("doc");
-let location = getLocation();
-const units = "metric";
-var lon = document.getElementById('longitude').value;
-var lat = document.getElementById('latitude').value;
-let lang = 'en';
-let url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid="+ key;
-
-function getWeather(url) {
-    fetch(url)
-        .then((response) => response.json()) // gets weather data and represents it in json format
-        .then((data) => console.log(data)); // displays data from api
+getWeather();
+function getWeather(){
+ getLocation(); //Gets Current Location & returns weather
+  
 }
-
 
 function getLocation() {
-   if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+   if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(showPosition);
 
-    } else 
-      { 
-        doc.innerHTML ="Geolocation is not supported";
+   
+  } else {
+   console.log("Gelocation is not available");
   }
+
 }
-
-
-function getCity(){
-   return navigator.geolocation.getCity(getLocation);
-}
-
 
 function showPosition(position) {
-    doc.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br> Longitude: " + position.coords.longitude;
+  longitude = position.coords.longitude;
+  latitude = position.coords.latitude;
+    console.log("Latitude: " + position.coords.latitude +
+    " Longitude: " + position.coords.longitude);  
+
+  fetch("https://api.openweathermap.org/data/2.5/weather?lon=" + longitude + "&lat="+ latitude+ "&appid="+ key +"&units=metric")
+  .then((response) => response.json()) // gets weather data and represents it in json format
+  .then((data) => this.displayWeather(data)) // displays data from api
+
+}
+
+function displayWeather(json){
+const {name} = json;
+const {description,icon} = json.weather[0];
+const {temp, feels_like} = json.main;
+const {speed} = json.wind;
+const{sunset,sunrise} = json.sys;
+const date = new Date().toTimeString().split(" ")[0];
+
+
+const sun_set = new Date(sunset*1000).toTimeString().split(" ")[0];
+const sun_rise = new Date(sunrise*1000).toTimeString().split(" ")[0];
+
+console.log(name, description, icon, temp, feels_like, speed, date, sunset,sunrise);
+
+document.querySelector(".location").innerText = "Weather in " + name;
+document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+document.querySelector(".description").innerText = description.toUpperCase();
+document.querySelector(".temp").innerText = "Temp: " + temp + "°C";
+document.querySelector(".feelsLike").innerText = "Feels Like: " + feels_like + "°C";
+document.querySelector(".wind").innerText = "Wind: " + speed + " mph";
+document.querySelector(".lastUpdated").innerText = "Last Updated: " + date;
+document.querySelector(".sunrise").innerText = "Sunrise: " + sun_rise;
+document.querySelector(".sunset").innerText = "Sunset: " + sun_set;
+}
+
+// document.querySelector(".search button").addEventListener("click", function(){
+//  search();
+// }
+// )
+
+document.querySelector(".search-btn").addEventListener("click", function(){
+  search();
+}
+);
+
+document.querySelector(".search-bar").addEventListener('keyUp', function(event){
+  if (event.key == "Enter") {
+     search();
   }
+  else {
+    console.log('fail');
+  }
+}
+);
 
-  //Last updated
-var date = new Date();
-console.log(date);
+function search(){
+ this.getCityWeather(document.querySelector(".search-bar").value);
+};
 
-//var time = date.getTime();
-//console.log(time);
+function getCityWeather(city){
 
-//api call for a city: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1"+"&appid=" + key
+  )
+  .then((response) => response.json()) // gets weather data and represents it in json format
+  .then((data) => this.getCoords(data)) // displays data from api
+
+};
+
+function getCoords(json){
+  const {lat} = json[0];
+  const {lon} = json[0];
+ latitude = lat;
+ longitude = lon;
+ key = "de9bd6f99b628a122f3be3f98428c81c";
+
+ fetch("https://api.openweathermap.org/data/2.5/weather?lon=" + longitude + "&lat="+ latitude+ "&appid="+ key +"&units=metric"
+  )
+  .then((response) => response.json()) // gets weather data and represents it in json format
+  .then((data) => this.displayWeather(data)) // displays data from api
+
+}
 
 
